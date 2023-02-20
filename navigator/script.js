ad46573a6c28f45d5defb91b943153230103c7db
Thomas Wright <37813874+thomasii@users.noreply.github.com>
2023-02-20 13:46:48 -0500

    Delete navigator directory

--- navigator/script.js
+++
@@ -1,7 +0,0 @@
-$('#search-form').on('submit', function(event) {
-	event.preventDefault();
-	var query = $('#search-input').val();
-	if (query) {
-		window.location.href = 'https://www.google.com/search?q=' + query;
-	}
-});
