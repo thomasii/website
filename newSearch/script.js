e2c9b4b7e295a90717165dcd34dec9a4dd6b748e
Thomas Wright <thomaswright1318@gmail.com>
2023-03-06 09:28:34 -0500

    newsearchBox

--- newSearch/script.js
+++ newSearch/script.js
@@ -1,47 +1,64 @@
-// JavaScript code for the search bar
 const data = [
-    { key: "German Shepherd", value: "https://en.wikipedia.org/wiki/German_Shepherd" },
-    { key: "Golden Retriever", value: "https://en.wikipedia.org/wiki/Golden_Retriever" },
-    { key: "Labrador Retriever", value: "https://en.wikipedia.org/wiki/Labrador_Retriever" },
-    { key: "Poodle", value: "https://en.wikipedia.org/wiki/Poodle" },
-    { key: "Siberian Husky", value: "https://en.wikipedia.org/wiki/Siberian_Husky" }
-  ];
-  
-  const searchInput = document.getElementById("search-input");
-  const searchResults = document.getElementById("search-results");
-  const submitBtn = document.getElementById("submit-btn");
-  
-  let filteredData = [];
-  
-  function renderResults() {
-    // Clear previous search results
-    searchResults.innerHTML = "";
-    // Render filtered data as search results
-    filteredData.forEach(item => {
-      const li = document.createElement("li");
-      const a = document.createElement("a");
-      a.href = item.value;
-      a.innerText = item.key;
-      li.appendChild(a);
-      li.addEventListener("click", () => {
-        searchInput.value = item.key;
-        submitBtn.disabled = false;
-        searchResults.innerHTML = "";
-      });
-      searchResults.appendChild(li);
+  { key: "Poodle", value: "https://en.wikipedia.org/wiki/Poodle" },
+  { key: "Labrador Retriever", value: "https://en.wikipedia.org/wiki/Labrador_Retriever" },
+  { key: "German Shepherd", value: "https://en.wikipedia.org/wiki/German_Shepherd" },
+  { key: "Bulldog", value: "https://en.wikipedia.org/wiki/Bulldog" },
+  { key: "Beagle", value: "https://en.wikipedia.org/wiki/Beagle" },
+  { key: "Rottweiler", value: "https://en.wikipedia.org/wiki/Rottweiler" }
+];
+
+const searchInput = document.getElementById("search-input");
+const searchResults = document.getElementById("search-results");
+const submitBtn = document.getElementById("submit-btn");
+
+let filteredData = [];
+
+function renderResults() {
+  // Clear previous search results
+  searchResults.innerHTML = "";
+  // Render filtered data as search results
+  filteredData.forEach(item => {
+    const li = document.createElement("li");
+    li.innerText = item.key;
+    li.addEventListener("click", () => {
+      searchInput.value = item.key;
+      submitBtn.disabled = false;
+      searchResults.innerHTML = "";
     });
+    searchResults.appendChild(li);
+  });
+}
+
+function handleSearch() {
+  // Filter data based on user input
+  const userInput = searchInput.value.toLowerCase().trim();
+  
+  if (userInput === '') { // Show all options if search input is empty
+    filteredData = data;
+  } else {
+    filteredData = data.filter(item => item.key.toLowerCase().includes(userInput));
   }
   
-  function handleSearch() {
-    // Filter data based on user input
-    const userInput = searchInput.value.toLowerCase().trim();
-    filteredData = data.filter(item => item.key.toLowerCase().startsWith(userInput));
-    // Render search results
+  // Render search results
+  renderResults();
+}
+
+
+function handleClickOutside(event) {
+  if (!searchInput.contains(event.target) && !searchResults.contains(event.target)) {
+    filteredData = [];
     renderResults();
   }
-  
-  // Add event listeners to search input and submit button
-  searchInput.addEventListener("input", handleSearch);
-  submitBtn.addEventListener("click", () => {
-    window.location.href = filteredData[0].value;
-  });
+}
+
+// Add event listeners to search input and submit button
+searchInput.addEventListener("input", handleSearch);
+submitBtn.addEventListener("click", () => {
+  const userInput = searchInput.value.toLowerCase().trim();
+  const result = data.find(item => item.key.toLowerCase() === userInput);
+  if (result) {
+    window.location.href = result.value;
+  }
+});
+
+document.addEventListener("click", handleClickOutside);
