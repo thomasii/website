95e5bb4e1f631c1e8bc45bb46b2359b3971563bc
Thomas Wright <thomaswright1318@gmail.com>
2023-03-21 12:32:51 -0400

    Yes

--- data1.js
+++ data1.js
@@ -1,8 +1,26 @@
-const data = [
-    { key: "Poodle", value: "https://en.wikipedia.org/wiki/Poodle" },
-    { key: "Labrador Retriever", value: "https://en.wikipedia.org/wiki/Labrador_Retriever" },
-    { key: "German Shepherd", value: "https://en.wikipedia.org/wiki/German_Shepherd" },
-    { key: "Bulldog", value: "https://en.wikipedia.org/wiki/Bulldog" },
-    { key: "Beagle", value: "https://en.wikipedia.org/wiki/Beagle" },
-    { key: "Rottweiler", value: "https://en.wikipedia.org/wiki/Rottweiler" }
-  ];
+[
+  {
+    "key": "Poodle",
+    "value": "https://en.wikipedia.org/wiki/Poodle"
+  },
+  {
+    "key": "Labrador Retriever",
+    "value": "https://en.wikipedia.org/wiki/Labrador_Retriever"
+  },
+  {
+    "key": "German Shepherd",
+    "value": "https://en.wikipedia.org/wiki/German_Shepherd"
+  },
+  {
+    "key": "Bulldog",
+    "value": "https://en.wikipedia.org/wiki/Bulldog"
+  },
+  {
+    "key": "Beagle",
+    "value": "https://en.wikipedia.org/wiki/Beagle"
+  },
+  {
+    "key": "Rottweiler",
+    "value": "https://en.wikipedia.org/wiki/Rottweiler"
+  }
+]
