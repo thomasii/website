fd7b8b10703e9877db848e9a6861917e3789dc54
Thomas Wright <thomaswright1318@gmail.com>
2023-03-21 13:48:39 -0400

    yess

--- newSearch/data2.js
+++ newSearch/data2.js
@@ -1,26 +1,26 @@
 [
   {
-    "key": "PoodleTEST",
+    "key": "Poodle TEST",
     "value": "https://en.wikipedia.org/wiki/Poodle"
   },
   {
-    "key": "Labrador Retriever",
+    "key": "Labrador Retriever TEST",
     "value": "https://en.wikipedia.org/wiki/Labrador_Retriever"
   },
   {
-    "key": "German Shepherd",
+    "key": "German Shepherd TEST",
     "value": "https://en.wikipedia.org/wiki/German_Shepherd"
   },
   {
-    "key": "Bulldog",
+    "key": "Bulldog TEST",
     "value": "https://en.wikipedia.org/wiki/Bulldog"
   },
   {
-    "key": "Beagle",
+    "key": "Beagle TEST",
     "value": "https://en.wikipedia.org/wiki/Beagle"
   },
   {
-    "key": "Rottweiler",
+    "key": "Rottweiler TEST",
     "value": "https://en.wikipedia.org/wiki/Rottweiler"
   }
 ]
--- newSearch/script.js
+++ newSearch/script.js
@@ -1,64 +1,73 @@
-const searchInput = document.getElementById("search-input");
-const searchResults = document.getElementById("search-results");
-const submitBtn = document.getElementById("submit-btn");
+searchContainers = document.querySelectorAll(".search-container");
 
-let filteredData = [];
+searchContainers.forEach((searchContainer) => {
+  const searchInput = searchContainer.querySelector("#search-input");
+  const searchResults = searchContainer.querySelector("#search-results");
+  const submitBtn = searchContainer.querySelector("#submit-btn");
+  let filteredData = [];
+  let data = [];
 
-let data = []; // initialize data as an empty array
-
-// Load data from JSON file
-fetch(document.querySelector('.search-container').getAttribute('data-datafile'))
-  .then(response => response.json())
-  .then(json => {
-    data = json; // assign retrieved data to the data variable
-  });
-
-function renderResults() {
-  // Clear previous search results
-  searchResults.innerHTML = "";
-  // Render filtered data as search results
-  filteredData.forEach(item => {
-    const li = document.createElement("li");
-    li.innerText = item.key;
-    li.addEventListener("click", () => {
-      searchInput.value = item.key;
-      submitBtn.disabled = false;
-      searchResults.innerHTML = "";
+  // Load data from JSON file
+  fetch(searchContainer.getAttribute("data-datafile"))
+    .then((response) => response.json())
+    .then((json) => {
+      data = json;
     });
-    searchResults.appendChild(li);
-  });
-}
 
-function handleSearch() {
-  // Filter data based on user input
-  const userInput = searchInput.value.toLowerCase().trim();
-  
-  if (userInput === '') { // Show all options if search input is empty
-    filteredData = data;
-  } else {
-    filteredData = data.filter(item => item.key.toLowerCase().includes(userInput));
+  function renderResults() {
+    // Clear previous search results
+    searchResults.innerHTML = "";
+    // Render filtered data as search results
+    filteredData.forEach((item) => {
+      const li = document.createElement("li");
+      li.innerText = item.key;
+      li.addEventListener("click", () => {
+        searchInput.value = item.key;
+        submitBtn.disabled = false;
+        searchResults.innerHTML = "";
+      });
+      searchResults.appendChild(li);
+    });
   }
-  
-  // Render search results
-  renderResults();
-}
 
+  function handleSearch() {
+    // Filter data based on user input
+    const userInput = searchInput.value.toLowerCase().trim();
 
-function handleClickOutside(event) {
-  if (!searchInput.contains(event.target) && !searchResults.contains(event.target)) {
-    filteredData = [];
+    if (userInput === "") {
+      // Show all options if search input is empty
+      filteredData = data;
+    } else {
+      filteredData = data.filter((item) =>
+        item.key.toLowerCase().includes(userInput)
+      );
+    }
+
+    // Render search results
     renderResults();
   }
-}
 
-// Add event listeners to search input and submit button
-searchInput.addEventListener("input", handleSearch);
-submitBtn.addEventListener("click", () => {
-  const userInput = searchInput.value.toLowerCase().trim();
-  const result = data.find(item => item.key.toLowerCase() === userInput);
-  if (result) {
-    window.location.href = result.value;
+  function handleClickOutside(event) {
+    if (
+      !searchInput.contains(event.target) &&
+      !searchResults.contains(event.target)
+    ) {
+      filteredData = [];
+      renderResults();
+    }
   }
-});
 
-document.addEventListener("click", handleClickOutside);
+  // Add event listeners to search input and submit button
+  searchInput.addEventListener("input", handleSearch);
+  submitBtn.addEventListener("click", () => {
+    const userInput = searchInput.value.toLowerCase().trim();
+    const result = data.find(
+      (item) => item.key.toLowerCase() === userInput
+    );
+    if (result) {
+      window.location.href = result.value;
+    }
+  });
+
+  document.addEventListener("click", handleClickOutside);
+});
