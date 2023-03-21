afaad68118f7168e6e5131ee0bcd885f17566cfa
Thomas Wright <thomaswright1318@gmail.com>
2023-03-21 15:41:46 -0400

    who

---
+++ comp.js
@@ -0,0 +1,26 @@
+[
+  {
+    "key": "My Wellbeing",
+    "value": "https://www.google.com"
+  },
+  {
+    "key": "ComPsych Mental Health Resources",
+    "value": "https://www.google.com"
+  },
+  {
+    "key": "Online Therapy",
+    "value": "https://www.google.com"
+  },
+  {
+    "key": "NGC manager help line",
+    "value": "https://www.google.com"
+  },
+  {
+    "key": "Education Assistance",
+    "value": "https://www.google.com"
+  },
+  {
+    "key": "Total Rewards",
+    "value": "https://www.google.com"
+  }
+]
